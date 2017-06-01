2a5da8b06f594cbd3714cf4ef9c20e96730b7f82
Sergio Martin Alarcón Montero <sergiom_malarcon@yahoo.com>
2017-06-01 17:19:04 -0400

    Archivos de configuración IdEn Framework
    Se cargaron los archivos del IdEn Framework, la ultima versión, se
    corrigieron los archivos del bootstrap, font-awesome y la página
    principal del sitio Enlaceme.com

---
+++ idensystem/idenSetUp.php
@@ -0,0 +1,20 @@
+<?php
+define('DEFAULT_URL_HTACCESS','vsocialnetwork');
+
+define('DEFAULT_CONTROLLER','index');/* CONTROLADOR POR DEFECTO DE LA APLICACION */
+define('BASE_VIEW_URL','http://localhost/www.enlaceme.com/');/* RUTA POR DEFECTO DE LA VISTAS */
+define('DEFAULT_VIEW_LAYOUT','assets');/* VISTA POR DEFECTO DEL FRAMEWORK IDEN*/
+
+define('DEFAULT_SESSION_USER_TIME',60);
+define('DEFAULT_HASH_KEY','W*9X|f6o]2;c');//http://clavesegura.org/
+
+define('DEFAULT_USER_REGISTER','user');
+define('DEFAULT_USER_AUTHENTICATE','vAuthenticatedEnlacemeUser');
+
+/*define('DEFAULT_DB_HOST','localhost');
+define('DEFAULT_DB_ROOT_USER','root');
+define('DEFAULT_DB_ROOT_PASS','vertrigo');
+define('DEFAULT_DB_NAME','db_idenframework');*/
+
+define('DEFAULT_DB_CHARSET','utf8');
+?>
---
+++ views/frontend/index/index.php
@@ -0,0 +1,227 @@
+<!-- GoogleAnalytic -->  
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-55420692-1', 'auto');
+  ga('send', 'pageview');
+
+if (top.location!= self.location)
+{
+top.location = self.location
+}
+</script>
+<!DOCTYPE html>
+<html lang="es">
+	<!-- BEGIN HEAD -->
+	<head>
+		<meta charset="utf-8"/>
+		<title>Red Social Enlaceme</title>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta content="width=device-width, initial-scale=1" name="viewport"/>
+		<meta content="" name=""/>
+		<meta content="" name=""/>
+
+		<meta name="description" content="Red social de trabajo, para personas profesionales, técnicos o que realizan un oficio."/>
+		<link rel="canonical" href="http://enlaceme.com/" />
+		<meta property="og:locale" content="es_ES" />
+		<meta property="og:type" content="website" />
+		<meta property="og:title" content="Red Social Enlaceme" />
+		<meta property="og:description" content="Red social de trabajo, para personas profesionales, técnicos o que realizan un oficio." />
+		<meta property="og:url" content="http://enlaceme.com/" />
+		<meta property="og:site_name" content="Red Social Enlaceme" />
+		<meta property="article:publisher" content="https://www.facebook.com/redsocialenlaceme" />
+		<meta name="twitter:card" content="summary_large_image"/>
+		<meta name="twitter:description" content="Red social de trabajo, para personas profesionales, técnicos o que realizan un oficio."/>
+		<meta name="twitter:title" content="Red Social Enlaceme"/>
+		<meta name="twitter:domain" content="Enlaceme"/>
+
+		<!--End of Zopim Live Chat Script-->
+		<!-- BEGIN GLOBAL MANDATORY STYLES -->
+		<link href='http://fonts.googleapis.com/css?family=Roboto:100,400,300,500,700' rel='stylesheet' type='text/css'>
+		<!--<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css"/>-->
+		<link href="<?php echo $vParamsViewBootstrap['root_bootstrap_css'];?>bootstrap.min.css" rel="stylesheet" type="text/css"/>
+		<link href="<?php echo $vParamsViewFrontEndLayout['root_frontend_plugins'];?>simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $vParamsViewBackEndLayout['root_backend_global_plugins'];?>font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+		<!-- END GLOBAL MANDATORY STYLES -->
+		<!-- BEGIN THEME STYLES -->
+		<link href="<?php echo $vParamsViewFrontEndLayout['root_frontend_css'];?>style.css" rel="stylesheet" type="text/css"/>
+		<!-- END THEME STYLES -->
+	</head>
+	<!-- END HEAD -->
+	<!-- BEGIN BODY -->
+	<body quick-markup_injected="true">
+		<!-- BEGIN TOPBAR -->
+		<div class="topbar navbar-fixed-top">
+			<a class="topbar-logo" href="http://www.enlaceme.com">
+				<img class="logo-200" src="<?php echo $vParamsViewFrontEndLayout['root_frontend_img'];?>logo-text-white.svg" alt="Logo">
+			</a>
+
+			<!--<a class="topbar-purchase" data-hint="tooltip" data-original-title="Ingresa o registrate gratuitamente!" data-placement="bottom" href="<?Php echo BASE_VIEW_URL; ?>login" role="button">Ingresar</a>-->
+
+		</div>
+		<!-- END TOPBAR -->
+		<!-- BEGIN BANNER -->
+		<div class="banner">
+			<div class="container">
+				<a data-hint="tooltip" data-original-title="Registrate ahora mismo!" data-placement="top" href="">
+				    <img class="logo-text-60" src="<?php echo $vParamsViewFrontEndLayout['root_frontend_img'];?>logo-text-white.svg" class="product-logo" align="center">
+                </a>
+				<h1 class="banner-title-main">
+                    <span class="banner-title-bold color-gray-1">Red social de <b>trabajo</b>, para <b>profesionales</b>, <b>técnicos</b> y/o <b>personas de oficio.</span>
+                </h1>
+				<!--<a data-hint="tooltip" data-original-title="Registrate ahora mismo!" data-placement="top" href="">
+				    <span class="banner-quote">Registro totalmente gratuito</span>
+				</a>-->
+			</div>
+			<!--<div class="container" style="margin-top: 30px;">
+				<center><h3 style="font-weight: 300">Vea un video de que hacemos nosotros</h3></center>
+				<div class="youtube" id="vKTq9s3NI44" data-params="modestbranding=1&showinfo=0&controls=1&vq=hd720" style="margin: 20px auto 30px auto; width:840px;height:460px;"></div>
+			</div>-->
+		</div>
+		<!-- END BANNER -->
+		<!-- BEGIN WIDGET BANNER -->
+		<div class="widget-banner">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 margin-bottom-30">
+						<div class="widget-body">
+							<div class="widget-layout">
+								<h3 class="widget-title"><span class="widget-subtitle color-gray-1">Profesionales</span></h3>
+							</div>
+							<a target="_blank" href="#">
+                                <img class="img-responsive" src="<?php echo $vParamsViewFrontEndLayout['root_frontend_img'];?>profesionales.jpg" alt="">
+                            </a>
+						</div>
+					</div>
+					<div class="col-md-6 margin-bottom-30">
+						<div class="widget-body">
+							<div class="widget-layout">
+								<h3 class="widget-title"><span class="widget-subtitle color-gray-1">Técnicos</span></h3>
+							</div>
+							<a target="_blank" href="#">
+                                <img class="img-responsive" src="<?php echo $vParamsViewFrontEndLayout['root_frontend_img'];?>tecnicos.jpg" alt="">
+                            </a>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6 margin-bottom-30">
+						<div class="widget-body">
+							<div class="widget-layout">
+								<h3 class="widget-title"><span class="widget-subtitle color-gray-1">Oficio</span></h3>
+							</div>
+							<a target="_blank" href="#">
+                                <img class="img-responsive" src="<?php echo $vParamsViewFrontEndLayout['root_frontend_img'];?>ocupacion.jpg" alt="">
+                            </a>
+						</div>
+					</div>
+					<div class="col-md-6 margin-bottom-30">
+						<div class="widget-body">
+							<div class="widget-layout">
+								<h3 class="widget-title"><span class="widget-subtitle color-gray-1">Pasatiempos</span></h3>
+							</div>
+							<a target="_blank" href="#">
+                                <img class="img-responsive" src="<?php echo $vParamsViewFrontEndLayout['root_frontend_img'];?>otros.jpg" alt="">
+                            </a>
+						</div>
+					</div>
+				</div>                
+			</div>
+		</div>
+		<!-- END WIDGET BANNER -->
+		<!-- BEGIN CUSTOMER FEEDBACK -->
+		<div class="container">
+			<div class="customer-feedback">
+				<center>
+					<h3 class="title-normal" style="margin-bottom: 50px;">¿Qué es lo que hacemos realmente?</h3>
+				</center>
+				<div style="">
+					<p class="customer-quote">
+                        Enlaceme.com, es un portal web, diseñado exclusivamente sobre los principios de una red social; Orientada aquellas personas que deseen brindar servicios, profesionales, técnicos de oficio o simplemente de pasatiempo como ingreso económico extra; Principalmente deseamos facilitar el registro de información sobre el trabajo o especialidad que las personas realizán y desean promocionar estas habilidades, para que personas que están en busca de estos servicios puedan encontrar la solución a sus necesidades.
+					</p>
+					<!--<span class="customer-name">Sergio Martín Alarcón Montero, CEO</span>-->
+				</div>
+
+			</div>
+		</div>
+		<!-- END CUSTOMER FEEDBACK -->
+            
+		<!-- BEGIN WIDGET BANNER -->
+		<div class="widget-banner">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 margin-bottom-30">
+						<div class="widget-body">
+							<div class="widget-layout">
+								<h3 class="widget-title"><span class="widget-subtitle title-2 color-gray-1">Buscas</span></h3>
+							</div>
+							<a target="_blank" href="#">
+                                <img class="img-responsive" src="<?php echo $vParamsViewFrontEndLayout['root_frontend_img'];?>profesionales.jpg" alt="">
+                            </a>
+						</div>
+					</div>
+					<div class="col-md-4 margin-bottom-30">
+						<div class="widget-body">
+							<div class="widget-layout">
+								<h3 class="widget-title"><span class="widget-subtitle title-2 color-gray-1">Encuentras</span></h3>
+							</div>
+							<a target="_blank" href="#">
+                                <img class="img-responsive" src="<?php echo $vParamsViewFrontEndLayout['root_frontend_img'];?>tecnicos.jpg" alt="">
+                            </a>
+						</div>
+					</div>
+					<div class="col-md-4 margin-bottom-30">
+						<div class="widget-body">
+							<div class="widget-layout">
+								<h3 class="widget-title"><span class="widget-subtitle title-2 color-gray-1">Recomiendas</span></h3>
+							</div>
+							<a target="_blank" href="#">
+                                <img class="img-responsive" src="<?php echo $vParamsViewFrontEndLayout['root_frontend_img'];?>ocupacion.jpg" alt="">
+                            </a>
+						</div>
+					</div>                    
+				</div>                
+			</div>
+		</div>
+		<!-- END WIDGET BANNER -->
+            
+		<div style="background: #282828; padding: 80px 0 80px 0">
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						<div class="questions-header color-white">
+                            ¿Eres profesional independiente?, ¿Deseas hacer conocer tu trabajo?, ¿Tienes experiencia, pero no tienes un título?, ¿Eres estudiante y quieres practicar, además de ganar algo de dinero? ¿Simplemente, quieres algo extra?
+						</div>
+						<div class="questions-content color-white">
+                            <p>Si tu te haces todas estas preguntas y más, estas en el lugar correcto, esta es una red social de trabajo, donde podras registrar, cargar y mostrar con descripciones, fotografias, mapas y mucho más todo lo que haces, todo lo que puedes hacer y cual es el monto que cobras por el trabajo.</p>
+                            <p>Además, las personas que contraten tus servicios podrán realizar recomendaciones a otras personas, evaluando tu capacidad de trabajo, el trato que has brindado y lo mejor de todo valorará tu trabajo, agradeciendote!</p>
+                            <p>Y por favor, no lo olvides, esto no es solo para profesionales, tambien es para técnicos, personas que realizan algun oficio o simplemente para personas que deseen ganar dinero extra.</p>
+						</div>                        
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- BEGIN FOOTER -->
+		<footer class="footer">
+			<img class="footer-logo logo-200" src="<?php echo $vParamsViewFrontEndLayout['root_frontend_img'];?>logo-text-white.svg" alt="Logo">
+			<p class="copyright color-gray-2">2017 Copyright &copy; Derechos reservados <a class="color-gray-3" href="http://www.ideas-envision.com" target="_blank">Ideas-Envision Servicios Integrales</a></p>
+		</footer>
+		<!-- END FOOTER -->
+		<!-- BEGIN JAVASCRIPTS -->
+		<!-- BEGIN CORE PLUGINS -->
+		<script src="<?php echo $vParamsViewFrontEndLayout['root_frontend_plugins'];?>jquery-1.11.0.min.js" type="text/javascript"></script>
+		<script src="<?php echo $vParamsViewBootstrap['root_bootstrap_js'];?>bootstrap.min.js" type="text/javascript"></script>
+		<script src="<?php echo $vParamsViewFrontEndLayout['root_frontend_plugins'];?>jquery.youtube.js" type="text/javascript"></script>
+		<script>
+			$(document).ready(function(){
+				$('[data-hint="tooltip"]').tooltip({
+					container: 'body'
+				})
+			});
+		</script>
+	</body>
+	<!-- END BODY -->
+</html>
