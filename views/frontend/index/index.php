0fd12f091b3c885bb5faa0124a4baa2c30711e07
Sergio Martin Alarcón Montero <sergiom_malarcon@yahoo.com>
2017-07-08 11:21:46 -0400

    Modificaciones NavBar
    Se realizo la reprogramación html del navbar

--- views/frontend/index/index.php
+++ views/frontend/index/index.php
@@ -43,25 +43,50 @@ top.location = self.location
         <link type="text/css" rel="stylesheet" href="<?Php echo $vParamsViewMaterialize['root_materialize_css']; ?>materialize.min.css"  media="screen,projection"/>
     </head>
 
-    <body>
+    <body>        
         <nav>
             <div class="nav-wrapper">
-                <a href="#!" class="brand-logo"><img src="<?Php echo $vParamsViewBackEndLayout['root_backend_layouts_images']; ?>logo-text-white.svg" width="270px"></a>
-                <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-                <ul class="right hide-on-med-and-down">
-                    <li><a href="login">Ingresar</a></li>
-                    <li><a href="register">Registrarme</a></li>
-                    <li><a href="info">Información</a></li>
-                </ul>
-                <ul class="side-nav" id="mobile-demo">
-                    <li><a href="login">Ingresar</a></li>
-                    <li><a href="register">Registrarme</a></li>
-                    <li><a href="info">Información</a></li>
-                </ul>
+              <a href="#!" class="brand-logo"><img src="<?Php echo $vParamsViewBackEndLayout['root_backend_layouts_images']; ?>logo-text-white.svg" width="270px"></a>
+              <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+              <ul class="right hide-on-med-and-down">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">Javascript</a></li>
+                <li><a href="mobile.html">Mobile</a></li>
+              </ul>
+              <ul class="side-nav" id="mobile-demo">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">Javascript</a></li>
+                <li><a href="mobile.html">Mobile</a></li>
+              </ul>
             </div>
-        </nav>        
+        </nav>
+        
+
+
+
+        <div class="row">
+            <div class="col s12"><p>s12</p></div>
+            <div class="col s12 m4 l2"><p>s12 m4</p></div>
+            <div class="col s12 m4 l8"><p>s12 m4</p></div>
+            <div class="col s12 m4 l2"><p>s12 m4</p></div>
+        </div>
+        <div class="row">
+            <div class="col s12 m6 l3"><p>s12 m6 l3</p></div>
+            <div class="col s12 m6 l3"><p>s12 m6 l3</p></div>
+            <div class="col s12 m6 l3"><p>s12 m6 l3</p></div>
+            <div class="col s12 m6 l3"><p>s12 m6 l3</p></div>
+        </div>        
+        
         <!--<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>-->
         <script type="text/javascript" src="<?Php echo $vParamsViewBackEndLayout['root_backend_global_plugins']; ?>jquery-3.2.1.min.js"></script>
         <script type="text/javascript" src="<?Php echo $vParamsViewMaterialize['root_materialize_js']; ?>materialize.min.js"></script>
+        
+        <script>
+            $(document).ready(function(){
+                 $('.button-collapse').sideNav();
+            });
+        </script>
     </body>
 </html>
